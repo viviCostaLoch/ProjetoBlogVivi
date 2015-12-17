8a862ca79d83db3de96dceb1d5ab2b17f2bac174
vivi costa <guatagames.info@gmail.com>
2015-12-17 21:59:20 -0200

    alteração banco

--- BlogVivi.db/Infra/MeuCriadorDeBanco.cs
+++ BlogVivi.db/Infra/MeuCriadorDeBanco.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BlogVivi.db.Infra
 {
-  public  class MeuCriadorDeBanco : DropCreateDatabaseIfModelChanges<ConexaoBanco>
+  public  class MeuCriadorDeBanco : DropCreateDatabaseAlways<ConexaoBanco>
     {
         protected override void Seed(ConexaoBanco context)
         {
